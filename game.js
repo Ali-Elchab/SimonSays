7da768329aef6e7af8419a2604cee47b58f6700f
Ali Hadi Elchab <alielchab01@gmail.com>
2023-11-12 15:02:42 +0200

    Added playsound and effects functions

--- game.js
+++ game.js
@@ -16,6 +16,25 @@ function nextlevel() {
   effects(nextColor);
 }
 
+function playsound(color) {
+  let colorAudio = new Audio(`./sounds/${color}.mp3`);
+  colorAudio.play();
+}
+
+function effects(color) {
+  let colorBox = document.getElementById(color);
+  if (colorBox) {
+    colorBox.style.transition = "opacity 0.1s";
+    colorBox.style.opacity = 0;
+    colorBox.style.boxShadow = "0 0 15px 10px white";
+
+    setTimeout(function () {
+      colorBox.style.opacity = 1;
+      colorBox.style.boxShadow = "initial";
+    }, 120);
+  }
+}
+
 document.addEventListener("click", function () {
   if (level <= 0) {
     nextlevel();
